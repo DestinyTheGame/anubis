c380b86695b9ade0cd3f5ee22c7a5f213c65ab55
Arnout Kazemier <info@3rd-Eden.com>
2016-12-14 17:26:01 +0100

    [major] Use a preboot sequence to clean up our booting process

--- bootstrap.js
+++ bootstrap.js
@@ -1,52 +1,13 @@
-import electron, { BrowserWindow } from 'electron';
-import { gets } from './storage';
-import path from 'path';
-import url from 'url';
-
-const app = electron.app;
-let anubis;
-
-/**
- * Conditionally create a new Application window.
- *
- * @public
- */
-function activate() {
-  if (anubis) return;
-
-  gets('width', 'height', (err, config) => {
-    if (err) throw err;
-
-    anubis = new BrowserWindow({ width: config.width, height: config.height });
-    anubis.on('closed', () => {
-      anubis = null;
-    });
-
-    //
-    // Allow introspection of the application when running in DEV mode.
-    //
-    if (process.env.NODE_ENV === 'development') {
-      anubis.webContents.openDevTools();
-    }
-
-    //
-    // Load the actual application.
-    //
-    anubis.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  });
-}
+import server from './preboot/server';
+import oauth from './preboot/oauth';
+import anubis from './anubis';
+import boot from 'booting';
 
 //
-// OSX is being an odd ball here, it doesn't shutdown the app when you close
-// windows only when you explicitly close (CMD + Q) the application.
+// Simple boot sequence to start up all required components to get our
+// application working.
 //
-if (process.platform !== 'darwin') {
-  app.on('window-all-closed', app.quit);
-}
-
-app.on('activate', activate);
-app.on('ready', activate);
+boot(new Map())
+.use(server)
+.use(oauth)
+.start(anubis);
