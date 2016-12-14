e4892f7b3c50fd9ffa99c508f7fc3ccbebac2a92
Arnout Kazemier <info@3rd-Eden.com>
2016-12-14 17:25:10 +0100

    [fix] Allow `bungie-auth` to be transformed

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 //
 // Bootstrap the application with babel so we can write it in ES6.
 //
-require('babel-register');
+require('babel-register')({
+  ignore: function ignore(filename){
+    var node_modules = !!~filename.indexOf('node_modules');
+
+    if (~filename.indexOf('bungie-auth') && !~filename.indexOf('bungie-auth/node_modules')) {
+      return false;
+    }
+
+    return node_modules;
+  }
+});
+
 require('./bootstrap.js');
