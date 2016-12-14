c2a8c29fd0d1344d27dc4f753c8bb0f7b26ccf3c
Arnout Kazemier <info@3rd-Eden.com>
2016-12-14 17:27:22 +0100

    [minor] Basic websocket connection accepting

---
+++ websocket.js
@@ -0,0 +1,10 @@
+/**
+ * Interact with out WebSocket connection to handle communication between server
+ * and client.
+ *
+ * @param {WebSocket} client Incoming WebSocket connection.
+ * @private
+ */
+export default function websocket(client) {
+
+};
