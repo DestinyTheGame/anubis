5ef0cfcc1c9b0efe1fd8d2ed2e08c67617270d5f
Arnout Kazemier <info@3rd-Eden.com>
2016-12-15 19:59:43 +0100

    [major] Use `babel-ignore` to streamline compiling

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
+'use strict';
+
+var ignore = require('babel-ignore');
+
 //
 // Bootstrap the application with babel so we can write it in ES6.
 //
-require('babel-register')({
-  ignore: function ignore(filename){
-    var node_modules = !!~filename.indexOf('node_modules');
-
-    if (~filename.indexOf('bungie-auth') && !~filename.indexOf('bungie-auth/node_modules')) {
-      return false;
-    }
-
-    return node_modules;
-  }
-});
-
+require('babel-register')({ ignore: ignore });
 require('./bootstrap.js');
