6d9508c0c1e18eb83892d713c62e342f8b852d29
Arnout Kazemier <info@3rd-Eden.com>
2016-12-17 00:47:58 +0100

    [api] Broadcast activies to the UI

--- websocket.js
+++ websocket.js
@@ -1,3 +1,4 @@
+import TickTock from 'tick-tock';
 
 /**
  * Interact with out WebSocket connection to handle communication between server
@@ -7,10 +8,32 @@
  * @private
  */
 function incoming(boot) {
+  /**
+   * Handle incoming connections.
+   *
+   * @param {WebSocket} client Incoming WebSocket connection.
+   * @private
+   */
   return function connection(client) {
     const destiny = boot.get('destiny');
+    const timers = new TickTock();
 
-    console.log('recieved incomming connection');
+    timers.setInterval('advisors', () => {
+      destiny.go(() => {
+        destiny.characters[0].advisors((err, data) => {
+          if (err) return client.send(JSON.stringify({
+            error: err.message
+          }));
+
+          data.type = 'advisors';
+          client.send(JSON.stringify(data));
+        });
+      });
+    }, 10000);
+
+    client.on('close', () => {
+      timers.destroy();
+    });
   }
 }
 
