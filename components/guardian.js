417549edc169d96ab4503a59b4b12d94e5a74ea2
Arnout Kazemier <info@3rd-Eden.com>
2017-02-12 15:34:29 +0100

    [ui] Adding UI for guardian.gg lookup

--- components/guardian.js
+++ components/guardian.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Loading from 'halogen/PulseLoader';
 
 /**
  * Guardian.
@@ -7,14 +8,61 @@ import React, { Component, PropTypes } from 'react';
  * @public
  */
 export default class Guardian extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      results: Guardian.LOADING
+    };
+  }
+
+  /**
+   * Render the guardian.gg layout
+   *
+   * @returns {Component}
+   * @private
+   */
   render() {
+    let results;
+
+    if (this.state.results === Guardian.LOADING) {
+      results = (
+        <div className='loading'>
+          <Loading color='#eaedf3' />
+        </div>
+      );
+    } else {
+      results = (
+        <p>Guardian</p>
+      );
+    }
+
     return (
-      <div>
-        Guardian.
+      <div className='guardian'>
+        <form action='#'>
+          <fieldset>
+            <input type='text' placeholder='Username' />
+            <button type='submit'>
+              GO
+            </button>
+          </fieldset>
+        </form>
+
+        <div className='results'>
+          { results }
+        </div>
       </div>
     );
   }
-}
+};
+
+/**
+ * Various of loading / fetching states.
+ *
+ * @type {Number}
+ * @private
+ */
+Guardian.LOADING = 1;
 
 /**
  * Guardian Settings Page.
