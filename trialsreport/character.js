48af050e7eb962f78c8dcb1fb7a2e7a09d9d7fb5
Arnout Kazemier <info@3rd-Eden.com>
2017-02-24 03:28:58 +0100

    [major] Work with the destiny-api [minor] find the perks of the weapons.

--- trialsreport/character.js
+++ trialsreport/character.js
@@ -1,3 +1,46 @@
+const hideNodes = [
+  21956111,   // Orange Chroma
+  73039185,   // Blue Chroma
+  74523350,   // Cannibalism
+  193091484,  // Increase Strength
+  213547364,  // Will of Light
+  217480046,  // Twist Fate
+  300289986,  // Dreg Burn
+  431265444,  // Mutineer
+  472357138,  // Void Damage
+  519240296,  // White Chroma
+  617082448,  // Reforge Ready
+  643689081,  // Kinetic Damage
+  994456416,  // Burgeoning Hunger
+  1034209669, // Increase Intellect
+  1259277924, // Red Chroma
+  1263323987, // Increase Discipline
+  1270552711, // Infuse
+  1305317488, // Aspect Swap
+  1450441122, // Demotion
+  1516989546, // Magenta Chroma
+  1644354530, // Sword Strike
+  1782221257, // Shank Burn
+  1891493121, // Dark Breaker
+  1920788875, // Ascend
+  1975859941, // Solar Damage
+  2086308543, // Upgrade Defense
+  2133116599, // Deactivate Chroma
+  2470010183, // Hive Disruptor
+  2493487228, // Green Chroma
+  2688431654, // Arc Damage
+  2689436406, // Upgrade Damage
+  2845051978, // Ice Breaker
+  2978058659, // Oracle Disruptor
+  3200611139, // Scabbard
+  3575189929, // Hands-On
+  3707521590, // Vandal Burn
+  3742851299, // Lich Bane
+  3838454323, // Yellow Chroma
+  3985040583, // Disciplinarian
+  4197414939  // Inverse Shadow
+];
+
 /**
  * Character model.
  *
@@ -83,6 +126,41 @@ export default class Character {
   }
 
   /**
+   * Helper function for the equipped items to collect and decode all the perks
+   * on a given item.
+   *
+   * @param {Object} item Item definition.
+   * @returns {Array} The found nodes.
+   * @private
+   */
+  findPerks(item) {
+    const talentGrid = this.definitions.talentGrids[item.talentGridHash];
+    const nodes = [];
+
+    if (!talentGrid) return nodes;
+
+    item.nodes.forEach((node, index) => {
+      const definition = talentGrid.nodes[index];
+      const step = definition.steps[node.stepIndex];
+
+      if (
+           !(node.isActivated === true && definition.column > -1)
+        || !step
+        || !step.nodeStepName
+        || ~hideNodes.indexOf(step.nodeStepHash)
+      ) return;
+
+      nodes.push({
+        name: step.nodeStepName,
+        title: step.nodeStepDescription,
+        icon: step.icon
+      })
+    });
+
+    return nodes;
+  }
+
+  /**
    * Find the weapon that the user currently has equipped.
    *
    * @param {String} slot What slot should it be in.
@@ -90,15 +168,13 @@ export default class Character {
    * @public
    */
   equipped(slot) {
-    const bucket = this.slot(slot);
-    const item = this.inventory.items.filter((item) => {
-      //
-      // When we look up our self, we get _everything_ that is in our inventory,
-      // even shit that is not equipped. We don't want that. We only want to know
-      // what is equipped so we're going to ignore these items.
-      //
-      if (item.transferStatus !== 1) return false;
-      return item.bucketHash === bucket;
+    const bucketHash = this.slot(slot);
+    const items = this.inventory.buckets.Equippable
+
+    let item = items.filter((item) => {
+      if (!item.items[0]) return false;
+
+      return item.bucketHash === bucketHash;
     }).pop();
 
     //
@@ -107,16 +183,25 @@ export default class Character {
     //
     if (!item) return;
 
+
+    //
+    // Merge definition data with the inventory based item data. First we need
+    // to filter out the items that are in our inventory and only return the
+    // equipped weapons. This is only a requirement for when we search for our
+    // self because the API will return all weapons from our inventory instead
+    // of just the one we have equipped.
+    //
+    item = item.items.filter((item) => { return item.transferStatus === 1; })[0];
+    item = Object.assign(this.definitions.items[item.itemHash], item);
+
     const element = this.definitions.damageTypes[item.damageTypeHash];
-    const stats = Object.keys(item.stats).map((statHash) => {
-      const statDetails = this.definitions.stats[statHash];
-      const stat = item.stats[statHash];
+    const stats = item.stats.map((stat) => {
+      const statDetails = this.definitions.stats[stat.statHash];
 
       return {
         name: statDetails.statName,
         title: statDetails.statDescription,
-        minimum: stat.minimum,
-        maximum: stat.maximum,
+        maximum: stat.maximumValue,
         value: stat.value
       }
     });
@@ -132,7 +217,9 @@ export default class Character {
         name: element.damageTypeName,
         title: element.description,
         icon: element.iconPath
-      }
+      },
+
+      perks: this.findPerks(item)
     }
   }
 
