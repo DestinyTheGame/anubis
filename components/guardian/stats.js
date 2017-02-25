0d5966321ad85eff32fc3dc75a53c922c8dfd2a8
Arnout Kazemier <info@3rd-Eden.com>
2017-02-25 15:03:07 +0100

    [fix] Use function based appraoch instead of classes

--- components/guardian/stats.js
+++ components/guardian/stats.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Representation of a single stat bar.
  *
- * @constructor
+ * @param {Object} props Properties.
  * @private
  */
-export default class Stats extends Component {
-  render() {
-    const props = this.props;
-    const value = props.value;
+export default function Stats(props) {
+  const value = props.value;
 
-    return (
-      <div className='stats'>
-        <div className='fill' style={ { width: ((value / 10) * 100) +'%' } }></div>
+  return (
+    <div className='stats'>
+      <div className='fill' style={ { width: ((value / 10) * 100) +'%' } }></div>
 
-        <div className='info'>
-          <span className='value'>{ value }</span>
-          <span className='name'>{ props.name }</span>
-        </div>
+      <div className='info'>
+        <span className='value'>{ value }</span>
+        <span className='name'>{ props.name }</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
