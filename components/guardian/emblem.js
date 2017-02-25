97dddad429aac6feae3c33d87d8cc03e16ff48ba
Arnout Kazemier <info@3rd-Eden.com>
2017-02-25 15:02:54 +0100

    [fix] Use function based appraoch instead of class

--- components/guardian/emblem.js
+++ components/guardian/emblem.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Representation of a Emblem with user information.
  *
- * @constructor
+ * @param {Object} props Properties
  * @private
  */
-export default class Emblem extends Component {
-  render() {
-    const props = this.props;
-
-    return (
-      <div className='emblem' style={{'background-image':'url(https://www.bungie.net/' + props.background + ')'}} >
-        <img src={ 'https://www.bungie.net/' + props.icon } />
-        <span className='name'>{ props.name }</span>
-        <span className='class'>{ props.className }</span>
-        <span className='light'>{ props.lightlevel }</span>
-        <span className='kd'>{ props.kd }K/D</span>
-      </div>
-    );
-  }
+export default function Emblem(props) {
+  return (
+    <div className='emblem' style={{'backgroundImage':'url(https://www.bungie.net/' + props.background + ')'}} >
+      <img src={ 'https://www.bungie.net/' + props.icon } />
+      <span className='name'>{ props.name }</span>
+      <span className='class'>{ props.className }</span>
+      <span className='light'>{ props.lightlevel }</span>
+      <span className='kd'>{ props.kd }K/D</span>
+    </div>
+  );
 }
