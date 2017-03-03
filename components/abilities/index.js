efb0c8d73678f2a869bac4bac8e808757c724c93
Arnout Kazemier <info@3rd-Eden.com>
2017-03-03 19:48:20 +0100

    [minor] Added max class for maxed out. [minor] Change HTML to just div's

--- components/abilities/index.js
+++ components/abilities/index.js
@@ -1,48 +1,30 @@
+import classnames from 'classnames';
 import React from 'react';
 import './abilities.scss';
 
 /**
  * Render the users abilities.
  *
- * @param {Object} props Strength, Discipline and Intellect stats.
+ * @param {Object} props Stats and the name of the stat
  * @returns {Component} The user's abilities and cool downs.
  * @public
  */
 export default function abilities(props) {
-  const { intellect, discipline, strength } = props;
+  const { name, stat } = props;
+  const tier = classnames(tier, {
+    max: +stat.tier === 5
+  });
 
   return (
-    <div className='abilities'>
-      <dl>
-        <dt>Intellect</dt>
-        <dd>
-          T{ intellect.tier }
+    <div className='ability'>
+      <h4 className={ tier }>
+        T{ stat.tier }
+      </h4>
 
-          <span className='cooldown'>
-            { intellect.cooldown }
-          </span>
-        </dd>
-      </dl>
-      <dl>
-        <dt>Discipline</dt>
-        <dd>
-          T{ discipline.tier }
-
-          <span className='cooldown'>
-            { discipline.cooldown }
-          </span>
-        </dd>
-      </dl>
-      <dl>
-        <dt>Strength</dt>
-        <dd>
-          T{ strength.tier }
-
-          <span className='cooldown'>
-            { strength.cooldown }
-          </span>
-        </dd>
-      </dl>
+      <span className='cooldown'>
+        <strong>{ stat.cooldown }</strong>
+        <span className='short'>{ name }</span>
+      </span>
     </div>
   );
 }
