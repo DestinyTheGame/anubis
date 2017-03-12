e41a363c9164f8cc4d1e2b3dd828980e4747e398
Arnout Kazemier <info@3rd-Eden.com>
2017-03-12 12:40:42 +0100

    [minor] Weekly component

---
+++ components/weekly/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/**
+ * Render weekly stats.
+ *
+ * @param {Object} props Properties.
+ * @private
+ */
+export default function weekly(props) {
+  const { currentMap, currentWeek } = props;
+
+  return (
+    <div className='report'>
+    </div>
+  );
+}
