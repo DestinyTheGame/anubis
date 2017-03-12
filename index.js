2c279ad930768e58e97a2b715570c5b44b8bb368
Arnout Kazemier <info@3rd-Eden.com>
2017-03-12 22:46:12 +0100

    [minor] Allow logging to file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 var ignore = require('babel-ignore');
+var fs = require('fs');
+
+//
+// Additional debug information.
+//
+// process.env.DEBUG = 'bungie-auth*,destiny*';
+// require('diagnostics').to(fs.createWriteStream('/Users/V1/Projects/destinythegame/anubis/anubis.log'));
+//
 
 //
 // Bootstrap the application with babel so we can write it in ES6.
