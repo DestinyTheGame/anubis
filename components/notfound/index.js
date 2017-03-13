089a0508992d0e9c537bf63a8bb35f14eaf070fa
Arnout Kazemier <info@3rd-Eden.com>
2017-03-13 10:03:11 +0100

    [ui] Restyle the notfound page

--- components/notfound/index.js
+++ components/notfound/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import './notfound.scss';
 
 /**
  * Not Found.
@@ -9,9 +10,28 @@ import React, { Component, PropTypes } from 'react';
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
-        The development of this view has not started yet. If you are interested in
-        contributing to this app, give us a nudge at github.com/destinythegame/anubis
+      <div className='main'>
+        <div className='panel'>
+          <div className='not-found'>
+            <h2>Oh shit, this isn't done yet</h2>
+
+            <p>
+              The development of this page hasn't started yet. If this is something
+              you would love to see and want to have the priority bumped on this feature
+              reach out on our <a href='https://discord.gg/kXn2NmQ'>discord channel</a>.
+            </p>
+
+            <p>
+              If you are are developer and interested in working on this page or just
+              helping out in general.
+              Give us a poke at <a href='https://github.com/destinythegame/anubis'>destinythegame/anubis</a> on Github.
+            </p>
+          </div>
+        </div>
+
+        <div className='panel' style={{ width: '33%'}}>
+          <img src='./design/crucible.jpg' />
+        </div>
       </div>
     );
   }
