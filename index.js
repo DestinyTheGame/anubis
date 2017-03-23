66e7f0d80fc252bb8b89f02978a215f513be375c
Arnout Kazemier <info@3rd-Eden.com>
2017-03-23 01:13:20 +0100

    [debug] Write anubis.log to app directory

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var ignore = require('babel-ignore');
+var app = require('electron').app;
+var path = require('path');
 var fs = require('fs');
 
 //
-// Additional debug information.
+// Additional debug information should be written to the application directory.
 //
-// process.env.DEBUG = 'bungie-auth*,destiny*';
-// require('diagnostics').to(fs.createWriteStream('/Users/V1/Projects/destinythegame/anubis/anubis.log'));
-//
+if (process.env.NODE_ENV !== 'development') {
+  process.env.DEBUG = 'bungie-auth*,destiny*';
+  require('diagnostics').to(fs.createWriteStream(path.join(app.getAppPath(), 'anubis.log')));
+}
 
 //
 // Bootstrap the application with babel so we can write it in ES6.
